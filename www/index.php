c972d2cc7e41cc66a9460e59e867d46badd10148
dyedp39 (Douglas Dye) <dyedp39@yahoo.com>
2012-12-09 17:35:36 +0000

    Dec 12 modified index
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/totalreturnport@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,9 +43,10 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
+<p>The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
-<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
-
+<p>For more information contact dyedp39@yahoo.com </p>
+<p>To use the code in this project you must first load the tseries , and quantmod librarys.</p>
+<p>&nbsp;</p>
 </body>
 </html>
