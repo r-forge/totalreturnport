aac14165bbbd0d1256562a36a840d671a2c39d5e
dyedp39 (Douglas Dye) <dyedp39@yahoo.com>
2012-12-10 05:12:25 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/totalreturnport@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sixfund.R
+++ pkg/sixfund.R
@@ -1,5 +1,14 @@
-nl <- length(symbols)+1
+syms <- c("VBMFX","VGTSX","VTSMX","SPY","IEF","GLD")
+sixfund <-portfolio(syms,"2010-10-01")
+nl <- length(syms)+1
+#remove date column and create matrix
 px <- as.matrix(sixfund[,2:nl])
+#calculate minimum risk port folio
 r <- portfolio.optim(px)
+#roud off the weights
 r$pw <- round(r$pw,digits=2)
-names(r$pw) <-symbols
+names(r$pw) <-syms
+#caculate annual risk
+risk <-r$ps*255^.5
+#caculate portfolio return
+rtn <- r$pm^255
